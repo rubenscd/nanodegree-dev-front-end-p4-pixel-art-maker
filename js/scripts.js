af780b18a779404784684d84910c67e45178efcd
Rubens Cavalheiro <rubenscd@macbook-pro-de-rubens-cavalheiro.local>
2018-02-04 16:08:15 -0200

    reordenado, melhorado os comentários

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
 $(function() {
-	console.log('carregou o scripts.js');
+
+	// objeto grid com todas as acoes
+	let grid = {
+		colorBase: '#fff',
+		colorSelected: '#000',
+		rowsTotals: 0,
+		colsTotals: 0,
+		// createdGrid: [[]],
+		cleanedGrid: function() {
+			/* TODO: remove grid */
+			$('#pixelCanvas tr').remove();
+		},
+		makeGrid: function() {
+			/* TODO: chama a limpeza se caso existir um grid criado */
+			if ($('#pixelCanvas').find('tr')) grid.cleanedGrid();
+			let createdGrid = "";
+			for (let row = 0; row < Number(grid.rowsTotals); row++) {
+				// grid.createdGrid[row] = [];
+				createdGrid += '<tr id="linha-'+row+'">';
+				for (let col = 0; col < Number(grid.colsTotals); col++) {
+					// grid.createdGrid[row][col] = 'off';
+					createdGrid += '<td id="linha-'+row+'-coluna-'+col+'"></td>';
+				}
+				createdGrid += '</tr>';
+			}
+			/* TODO: cria o grid com os parametros repassados do form */
+			return $('#pixelCanvas').append(createdGrid);
+		},
+		paintingGrid: function(cell) {
+			/* TODO: executa a pintura com a cor escolhida */
+			$(cell).css('background-color',grid.colorSelected);
+		},
+		changeColor: function(color) {
+			/* TODO: muda a cor a ser aplicada */
+			grid.colorSelected = color;
+		}
+	};
 
 	// polyfill input color
 	if (!Modernizr.inputtypes.color) {
@@ -8,47 +44,8 @@ $(function() {
 		});
 	}
 
-	// Get form data
-	let grid = {
-		colorBase: '#fff',
-		colorSelected: '#000',
-		rowsTotals: 0,
-		colsTotals: 0,
-		// createdGrid: [[]],
-		cleanedGrid: function() {
-			$('#pixelCanvas tr').remove();
-		},
-		makeGrid: function() {
-			grid.cleanedGrid();
-			let createdGrid = "";
-			for (let row = 0; row < Number(grid.rowsTotals); row++) {
-				// grid.createdGrid[row] = [];
-				createdGrid += '<tr id="linha-'+row+'">';
-				for (let col = 0; col < Number(grid.colsTotals); col++) {
-					// grid.createdGrid[row][col] = 'off';
-					createdGrid += '<td id="linha-'+row+'-coluna-'+col+'"></td>';
-					// createdGrid += '<td id="linha-'+row+'-coluna-'+col+'" class="off">'+(row+1)+","+(col+1)+'</td>';
-				}
-				createdGrid += '</tr>';
-			}
-			return $('#pixelCanvas').append(createdGrid);
-		},
-		paintingGrid: function(cell) {
-			// console.log($(cell));
-			// if ($(cell).hasClass('on')) {
-			// 	$(cell).css('background-color',grid.colorBase);
-			// } else {
-			// 	$(cell).css('background-color',grid.colorSelected);
-			// }
-			$(cell).css('background-color',grid.colorSelected);
-			// console.log($(celula)[0].id);
-		},
-		changeColor: function(color) {
-			grid.colorSelected = color;
-		}
-	};
-
 	// Eventos
+	// evento previne o submit do form mas envia os valores do grid para o objeto
 	$("#sizePicker").on('submit', function(evt) {
 		evt.preventDefault();
 		const returnForm = $(this).serializeArray();
@@ -56,16 +53,12 @@ $(function() {
 		grid.colsTotals = returnForm[1].value;
 		grid.makeGrid();
 	});
+	// evento escuta se dentro do grid algum td foi clicado
 	$('#pixelCanvas').on('click','td', function(evt) {
 		grid.paintingGrid(evt.target);
 	});
+	// evento muda a cor
 	$('#colorPicker').on('change', function(evt){
-		// console.log(evt.target.value);
-		// console.log($('#colorPicker').spectrum("get"));
 		grid.changeColor(evt.target.value);
 	});
-	// $("#colorPicker").on('change', function(color) {
-	// 	console.log(color.toHexString());
-	// });
-	// console.log($('#colorPicker').spectrum("get"));
 });
